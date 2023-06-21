0b5a9b03eedab9015126ee6fa088978c9007d54d
Biuksum <midoymi@gmail.com>
2023-06-21 06:44:51 +0100

    Implementação dos itens no interface de Products

--- app/src/main/java/com/example/projectofinalmcm_ipg/MainActivity.kt
+++ app/src/main/java/com/example/projectofinalmcm_ipg/MainActivity.kt
@@ -4,23 +4,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.projectofinalmcm_ipg.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var bindiing: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+    var data = ArrayList<Products>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindiing = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindiing.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        bindiing.recyclerview.layoutManager = LinearLayoutManager(this)
+        binding.recyclerview.layoutManager = LinearLayoutManager(this)
 
         getAllProducts()
     }
 
     private fun getAllProducts() {
-        TODO("Not yet implemented")
+
+        val retrofit = ServiceBuilder.buildService(ServiceInterface::class.java)
+        retrofit.getAllProducts().enqueue(object : Callback<ApiResponse>{
+            override fun onResponse(call: Call<ApiResponse>, response: Response<ApiResponse>) {
+                try {
+                    // to avoid nulPointerException
+                    val responseBody = response.body()!!
+                    data = responseBody.products
+
+
+
+                }
+                catch (ex: java.lang.Exception){
+                    ex.printStackTrace()
+                }
+            }
+
+            override fun onFailure(call: Call<ApiResponse>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/projectofinalmcm_ipg/ServiceInterface.kt
@@ -0,0 +1,12 @@
+package com.example.projectofinalmcm_ipg
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface ServiceInterface {
+
+    @Headers("Content-Type:application/json")
+    @GET("products")
+    fun getAllProducts():Call<ApiResponse>
+}
