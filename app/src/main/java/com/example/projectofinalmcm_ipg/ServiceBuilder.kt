83a24509e8f3c3f57e3107280aeded69a121ea67
Biuksum <midoymi@gmail.com>
2023-06-21 06:07:44 +0100

    vcriar uma instância do objeto

--- app/src/main/java/com/example/projectofinalmcm_ipg/ServiceBuilder.kt
+++ app/src/main/java/com/example/projectofinalmcm_ipg/ServiceBuilder.kt
@@ -1,5 +1,20 @@
 package com.example.projectofinalmcm_ipg
 
-object ServiceBuilder {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
+object ServiceBuilder {
+    private val client = OkHttpClient.Builder().build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://dummyjson.com")
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(client)
+        .build()
+
+    //let's create an instance of this object
+    fun<T> buildService(service:Class<T>):T{
+        return retrofit.create(service)
+    }
 }
